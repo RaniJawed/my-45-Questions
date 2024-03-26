73806dae0c289bd4e5fe0760163cf3e0a7171f70
RaniJawed <161884303+RaniJawed@users.noreply.github.com>
2024-03-26 15:26:13 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,2 +1,3 @@
-let personName = "Rani Jawed"
-console.log("hello", personName, " would you like to learn some Python today?");
+let famousPerson = "Albert Einstein";
+let message = " once said, “A person who never made a mistake never tried anything new.”";
+console.log(famousPerson, message);
